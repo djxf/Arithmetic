b26370bc2cfd396c589bd14b3a2f529ca3ee85cd
djxf <1763380121@qq.com>
2022-04-09 23:54:27 +0800

    相关：前缀表达式(波兰式)，后缀表达式，中缀表达式。

---
+++ 算法学习代码/Code0409.java
@@ -0,0 +1,28 @@
+package 算法学习代码;
+
+
+/**
+ *
+ *
+ * 给定一个字符串，str表示一个公式，公式里面可能有整数，加减乘除符号和左右括号，返回公式计算的结果。
+ * 比如： 213 + 432 + 23 * 4 + (432 + 99) / 45.
+ *
+ *
+ * 相关：前缀表达式(波兰式)，后缀表达式，中缀表达式。
+ */
+public class Code0409 {
+    public static void main(String[] args) {
+
+
+    }
+
+    /**
+     * 计算字符串的表达式
+     * @param nums
+     * @return
+     *
+     */
+    public static int caculateNums(String nums) {
+        return 0;
+    }
+}
