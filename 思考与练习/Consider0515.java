5be5ee4623492853fa3cb9eb85f45be2f5740eb3
djxf <1763380121@qq.com>
2022-05-16 00:01:14 +0800

    思考View工作原理

---
+++ 思考与练习/Consider0515.java
@@ -0,0 +1,11 @@
+package 思考与练习;
+
+
+/**
+ * 05/15思考
+ * Android View工作原理。https://github.com/xfhy/Android-Notes/blob/master/Blogs/Android/%E7%B3%BB%E7%BB%9F%E6%BA%90%E7%A0%81%E8%A7%A3%E6%9E%90/%E6%AD%BB%E7%A3%95Android_View%E5%B7%A5%E4%BD%9C%E5%8E%9F%E7%90%86%E4%BD%A0%E9%9C%80%E8%A6%81%E7%9F%A5%E9%81%93%E7%9A%84%E4%B8%80%E5%88%87.md
+ *
+ *
+ */
+public class Consider0515 {
+}
