4911d3aaf3c240ab572fd72e720d95f14d518a6f
djxf <1763380121@qq.com>
2022-05-23 00:25:37 +0800

    contentProvider

---
+++ 思考与练习/Consider0523.java
@@ -0,0 +1,11 @@
+package 思考与练习;
+
+/**
+ *
+ *
+ * 思考：
+ *      ContentProvider:
+ *      https://github.com/xfhy/Android-Notes/blob/master/Blogs/Android/%E7%B3%BB%E7%BB%9F%E6%BA%90%E7%A0%81%E8%A7%A3%E6%9E%90/%E6%AD%BB%E7%A3%95Android_ContentProvider_%E5%90%AF%E5%8A%A8.md
+ */
+public class Consider0523 {
+}
